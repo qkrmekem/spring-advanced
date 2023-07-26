b429baf00223417f07ebb3dcc8d6b1e2444d3c1d
qkrmekem <magicofclown@naver.com>
2023-07-26 23:29:48 +0900

    로그 추격기 개발중 - 메소드 호출 깊이, 요청 구분 개발 필요

---
+++ advanced/src/main/java/hello/advanced/app/v0/OrderControllerV0.java
@@ -0,0 +1,18 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV0 {
+    // f2 누르면 에러 발생 장소로 커서 이동
+    private final OrderServiceV0 orderService;
+
+    @GetMapping("/v0/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+}
---
+++ advanced/src/main/java/hello/advanced/trace/TraceId.java
@@ -0,0 +1,46 @@
+package hello.advanced.trace;
+
+import java.util.UUID;
+
+public class TraceId {
+    // 르랜잭션id와 깊이를 가지고 있는 클래스
+
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int level) {
+        this.id = id;
+        this.level = level;
+    }
+
+    private String createId() {
+        // uuid의 길이가 너무 길기 때문에 8자만 따서 사용
+        return UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    public TraceId createNextId() {
+        return new TraceId(id, level + 1);
+    }
+
+    public TraceId createPreviousId() {
+        return new TraceId(id, level - 1);
+    }
+
+    public boolean isFirstLevel() {
+        return level == 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+}
+
