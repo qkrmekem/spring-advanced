dd135779a90079595bb04b26e5e043a5738750d7
qkrmekem <magicofclown@naver.com>
2023-08-17 23:51:55 +0900

    스프링이 제공하는 포인트컷, 멀티 어드바이저

--- proxy/src/main/java/hello/proxy/app/v1/OrderRepositoryV1Impl.java
+++ proxy/src/main/java/hello/proxy/app/v1/OrderRepositoryV1Impl.java
@@ -1,6 +1,6 @@
 package hello.proxy.app.v1;
 
-public class OrderRepositoryV1Impl implements OrderRepositoryV1{
+public class OrderRepositoryV1Impl implements OrderRepositoryV1 {
     @Override
     public void save(String itemId) {
         // 저장로직
